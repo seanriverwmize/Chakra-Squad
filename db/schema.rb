4de04bcf545803243cd7aa246763b83af612c884
Sean River <seanriverwmize@gmail.com>
2018-08-27 18:47:50 -0700

    Changed datatype of Price from string to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_011710) do
+ActiveRecord::Schema.define(version: 2018_08_28_014453) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_011710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
-    t.string "price"
+    t.float "price"
   end
 
 end
