3f5e18b160f48a9f4cba9fbc557660d0537168a4
Sean River <seanriverwmize@gmail.com>
2018-08-31 22:23:02 -0700

    Created carousel on landing_page, and shortened products/index.html.erb by taking out part of the view and turning it into a partial.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,8 @@ class SimplePagesController < ApplicationController
   def index
   end
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
+
   end
   def contact
   end
