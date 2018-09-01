322fb9a7365c223b102802c75252f508685f7dbe
Sean River <seanriverwmize@gmail.com>
2018-08-31 21:11:17 -0700

    Fixed missing  in latest migration

--- db/migrate/20180830053921_change_price_datatype_to_integer2.rb
+++ db/migrate/20180830053921_change_price_datatype_to_integer2.rb
@@ -1,5 +1,5 @@
 class ChangePriceDatatypeToInteger2 < ActiveRecord::Migration[5.2]
   def change
-    change_column :products, :price, :float
+    change_column :products, :price, :integer
   end
 end
