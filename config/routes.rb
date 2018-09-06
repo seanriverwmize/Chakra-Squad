e60170515ff20f07a8a88cf54b62dfcdacd8cb0b
Sean River <seanriverwmize@gmail.com>
2018-09-06 01:46:56 -0700

    5.1 Changed root page to simple_pages/index. Made all links in the app dynamic rails links. Added dynamic links to all product pictures.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
