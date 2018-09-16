97e019ee8bfd3d708d2f7e2e0f6ff71357e35f5c
Sean River <seanriverwmize@gmail.com>
2018-09-15 19:33:59 -0700

    changes to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_033942) do
+ActiveRecord::Schema.define(version: 2018_09_16_012139) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
