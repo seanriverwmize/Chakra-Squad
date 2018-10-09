8bdbd609684f9ad43d6cb0f337fe3fdfa693e109
Sean River <seanriverwmize@gmail.com>
2018-10-08 21:10:16 -0700

    error in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,9 +7,9 @@ class Ability
     can :read, :all
     if user.present?
       can :manage, Comment, user_id: user.id
-      if user.admin?
-        can :manage, :all
-      end
+    end
+    if user.admin?
+      can :manage, :all
     end
   end
 end
