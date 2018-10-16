040514dc91c905a4095acead60dc4fbe5579cb6e
Sean River <seanriverwmize@gmail.com>
2018-10-16 11:02:59 -0700

    fixed error in Payments Controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,7 @@ class PaymentsController < ApplicationController
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: @product.price * 100, # amount in cents, again
+        amount: {@product.price * 100}, # amount in cents, again
         currency: "usd",
         source: token,
         description: params[:stripeEmail]
