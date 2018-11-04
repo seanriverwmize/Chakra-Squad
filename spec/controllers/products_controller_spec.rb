67e2b0675729b05d055d2f684e382668a6ee004d
Sean River <seanriverwmize@gmail.com>
2018-11-03 20:06:42 -0700

    Added tests to products_controller_spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 describe ProductsController, type: :controller do
-  
+  before do
+    @product = FactoryBot.create(:product)
+    @user = FactoryBot.create(:user)
+    @admin = FactoryBot.create(:admin)
+  end
+  describe 'Product page is called' do
+    context 'when a user requests index page' do
+      it 'loads product page successfully' do
+        get :index
+        expect(response).to be_ok
+      end
+    end
+    context 'when a user requests show page' do
+      it 'loads show page successfully' do
+        get :show, params: {id: @product.id}
+        expect(response).to be_ok
+        expect(assigns(:product)).to eq @product
+      end
+    end
+    context 'when a product is created correctly' do
+      before :each do
+        sign_in @user
+      end
+      it 'redirects to the new product show page' do
+        post :create, product: attributes_for(:product)
+        response.should redirect_to product_path
+      end
+    end
+    context 'when a product is deleted' do
+      before :each do
+        sign_in @admin
+      end
+      it 'redirects to products index page' do
+        delete :destroy, {id: @product.id}
+        expect(response).to redirect_to(products_url)
+      end
+    end
+  end
 end
