0d646f23210acdf72ec640df96b37bcedb628758
Sean River <seanriverwmize@gmail.com>
2018-11-08 14:32:09 -0800

    fixed syntax error in User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-rclass UsersController < ApplicationController
+class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
