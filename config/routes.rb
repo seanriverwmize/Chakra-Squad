a0986eaf225880ef01d6c6fee12123422d98bbb4
Sean River <seanriverwmize@gmail.com>
2019-01-30 01:46:11 -0800

    new, cooler landing page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
 
   post 'payments/create'
