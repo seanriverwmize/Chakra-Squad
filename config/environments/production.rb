ab132f439b4d3516529a8992a4b79304dda01caa
Sean River <seanriverwmize@gmail.com>
2019-01-30 03:11:37 -0800

    removed edit to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Enigma::Application.configure do
-    config.assets.compile = true
-  end
   # Code is not reloaded between requests.
   config.cache_classes = true
 
